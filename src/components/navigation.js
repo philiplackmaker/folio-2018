6891c3973efbaf18c0eed0900d1785e920287df3
philiplackmaker <philip@potatolondon.com>
2018-07-17 15:19:10 +0100

    fgfsd:wq 1

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Link from 'gatsby-link'
 import styled from "styled-components";
-import '/Users/philip/development/folio-2018/src/pages/global.css';
 
 
 const Navigation = styled.div`
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Link from "gatsby-link"
 import styled from "styled-components";
-import './global.css'
 
 
 import Navigation from '../components/navigation'
