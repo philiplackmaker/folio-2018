94e7d04aabee0a1fc8e3cf37ebac02b2ed302f42
philiplackmaker <philip@potatolondon.com>
2018-07-17 11:50:35 +0100

    update nav

---
+++ src/components/navigation.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import styled from "styled-components";
+
+
+const Navigation = styled.div`
+    width: 100%;
+    height: 100px;
+    background-color: red;
+    margin: 0 auto;
+    display: flex;
+`;
+export default Navigation
+
