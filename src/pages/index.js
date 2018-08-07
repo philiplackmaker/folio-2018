8ac1128aed8f7f7a1f5f15fe5961844a4e9157a6
philiplackmaker <philip@potatolondon.com>
2018-08-07 16:09:00 +0100

    f:wq

--- src/pages/index.js
+++ src/pages/index.js
@@ -140,8 +140,7 @@ export const pageQuery = graphql`
                     ...GatsbyImageSharpFluid
                   }
                 }
-              }
-      
+              } 
           }
         }
       }
