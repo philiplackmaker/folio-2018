6f567099835ff6631fff984d2de0c2b886b4e1cf
philiplackmaker <philip@potatolondon.com>
2018-08-17 16:13:25 +0100

    i

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,62 +1,50 @@
-  module.exports = {
-    plugins: [
-      {
-        resolve: 'gatsby-plugin-mixpanel',
-        options: {
-          apiToken: 'd6e013f46ee1a5160a1dc40eaf3f3178', // required
-        },
+module.exports = {
+  plugins: [
+    {
+      resolve: 'gatsby-plugin-mixpanel',
+      options: {
+        apiToken: 'd6e013f46ee1a5160a1dc40eaf3f3178', // required
       },
-      {
-        resolve: 'gatsby-plugin-intercom-spa',
-        options: {
-          app_id: "d7vu7om5"
-        },
+    },
+    {
+      resolve: 'gatsby-plugin-intercom-spa',
+      options: {
+        app_id: "d7vu7om5"
       },
-      `gatsby-plugin-react-helmet`,
-      `gatsby-plugin-styled-components`,
-      `gatsby-plugin-sass`,
-      `gatsby-remark-copy-linked-files`,
-      `gatsby-transformer-sharp`,
-      `gatsby-plugin-sharp`,
-    
-      {
-        resolve: `gatsby-source-filesystem`,
-        options: {
-          path: `${__dirname}/src/pages`,
-          name: 'pages',
-        },
+    },
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-sass`,
+    `gatsby-remark-copy-linked-files`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+  
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: 'pages',
       },
-      {
-        resolve: 'gatsby-source-filesystem',
-        options: {
-          path: `${__dirname}/src/images`,
-          name: 'images',
-        },
-      },
-      {
-        resolve: `gatsby-transformer-remark`,
-        options: {
-          // In your gatsby-transformer-remark plugin array
-          plugins: [
-            {
-              resolve: `gatsby-remark-relative-images`,
-              options: {
-                name: "images" // Must match the source name ^
-              }
-            },
-            {
-              resolve: `gatsby-remark-images`,
-              options: {
-                maxWidth: 1080,
-              },
-            },
-            `gatsby-remark-emoji`,
-            'gatsby-remark-static-images',
-            'gatsby-remark-copy-linked-files',
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        // In your gatsby-transformer-remark plugin array
+        plugins: [
 
-          ],
-        },
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 1080,
+            },
+          },
+          `gatsby-remark-emoji`,
+          'gatsby-remark-static-images',
+          'gatsby-remark-copy-linked-files',
+
+        ],
       },
-      
-    ],
-  }
+    },
+    
+  ],
+}
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,51 +1,41 @@
-// import React from 'react';
-// import styled from 'styled-components';
-// import { graphql } from 'gatsby';
-// import Img from 'gatsby-image';
+import React from 'react';
+import styled from 'styled-components';
+import { graphql } from 'gatsby';
+import Img from 'gatsby-image';
 
 
-// const BackGroundGradient = styled.div`
-//   background-image: linear-gradient(72deg, red, #f06d06);
-//   height: 100vh;
-//   flex-flow: row wrap;
-//   justify-content: space-around;
-//   align-items: center;
-// `;
-// const ImageHolder = styled.div`
-//   width: 1000px;
-//   padding: 5%;
-// `;
+const BackGroundGradient = styled.div`
+  background-image: linear-gradient(72deg, red, #f06d06);
+  height: 100vh;
+  flex-flow: row wrap;
+  justify-content: space-around;
+  align-items: center;
+`;
+const ImageHolder = styled.div`
+  width: 1000px;
+  padding: 5%;
+`;
 
-// const NotFoundPage = ({ data }) => (
-//   <div>
-//    <BackGroundGradient>
-//       <h2>Fuck Off</h2> 
-//       <ImageHolder>
-//       <Img fluid={data.file.childImageSharp.fluid} />
-//       </ImageHolder>
-//     </BackGroundGradient>
-//   </div>
-// );
-
-// export const pageQuery = graphql`
-//   query {
-//     file(relativePath: { eq: "kitten.jpg" }) {
-//       childImageSharp {
-//         fluid(maxWidth: 1000) {
-//           ...GatsbyImageSharpFluid
-//         }
-//       }
-//     }
-//   }
-// `
-// export default NotFoundPage
-import React from 'react'
-
-const NotFoundPage = () => (
+const NotFoundPage = ({ data }) => (
   <div>
-    <h1>Fuck off</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+   <BackGroundGradient>
+      <h2>Fuck Off</h2> 
+      <ImageHolder>
+      <Img fluid={data.file.childImageSharp.fluid} />
+      </ImageHolder>
+    </BackGroundGradient>
   </div>
-)
+);
 
+export const pageQuery = graphql`
+  query {
+    file(relativePath: { eq: "kitten.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 1000) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
 export default NotFoundPage
