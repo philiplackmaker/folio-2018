b118bf5ecbe104f071845f54149323b9f91a6041
philiplackmaker <philip@potatolondon.com>
2018-08-17 15:58:28 +0100

    Updates

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,27 +6,36 @@ import Img from 'gatsby-image';
 
 const BackGroundGradient = styled.div`
   background-image: linear-gradient(72deg, red, #f06d06);
+  height: 100vh;
+  flex-flow: row wrap;
+  justify-content: space-around;
+  align-items: center;
+`;
+const ImageHolder = styled.div`
+  width: 1000px;
+  padding: 5%;
 `;
 
 const NotFoundPage = ({ data }) => (
   <div>
-    <Img fixed={data.file.childImageSharp.fixed} />
-    <BackGroundGradient>
-      tes
+   <BackGroundGradient>
+      <h2>Fuck Off</h2> 
+      <ImageHolder>
+      <Img fluid={data.file.childImageSharp.fluid} />
+      </ImageHolder>
     </BackGroundGradient>
   </div>
 );
 
-export const query = graphql`
+export const pageQuery = graphql`
   query {
-    file(relativePath: { eq: "src/images/kitten.jpg" }) {
-      childImageSharp{
-        fixed(width: 125, height: 125) {
-          ...GatsbyImageSharpFixed
+    file(relativePath: { eq: "kitten.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 1000) {
+          ...GatsbyImageSharpFluid
         }
       }
     }
   }
 `
-
 export default NotFoundPage
