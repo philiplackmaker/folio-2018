332ea941780f45ba05395b0168b63711b68fd493
philiplackmaker <philip@potatolondon.com>
2018-08-17 08:54:45 +0100

    40404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,36 @@
 import React from 'react'
+import Kitten from '../images/Kitten.jpg';
+import styled from "styled-components";
+
+
+const CatBackground = styled.div`
+background-image: url(${Kitten});
+height: 100vh;
+background-repeat: no-repeat;
+background-position: center; 
+background-size:1000px 750px;
+transition: transform 200ms ease-in-out;
+&:hover {
+  transform: scale(1.1);
+
+}
+`;
+
+const BackGroundGradient = styled.div`
+background-image: linear-gradient(72deg, red, #f06d06);
+
+`;
+
 
 const NotFoundPage = () => (
   <div>
+    <BackGroundGradient>
+    <CatBackground>
     <h1>Fuck off</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </CatBackground>
+    </BackGroundGradient>
+ 
+    
   </div>
 )
 
